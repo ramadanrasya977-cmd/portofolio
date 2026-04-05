4db24b19d4787aef9aee67e4152c14fb6f144f88
ramadanrasya977-cmd <ramadanrasya977@gmail.com>
2026-04-05 12:52:40 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-alert ("js nyambung") ;
-
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
+
+  const tombol = document.querySelector(".dark-btn");
+
+  if (document.body.classList.contains("dark-mode")) {
+    tombol.innerHTML = "☀️ Light Mode";
+  } else {
+    tombol.innerHTML = "🌙 Dark Mode";
+  }
 }
