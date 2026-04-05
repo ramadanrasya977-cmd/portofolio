c7e2bc0da2a95b1f84949798a7b7d96b0e372341
ramadanrasya977-cmd <ramadanrasya977@gmail.com>
2026-04-05 13:21:21 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 alert ("nyambung");
 
-function toggleDarkMode() {
+window.toggleDarkMode = function () {
   document.body.classList.toggle("dark-mode");
-}
+
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("darkMode", "on");
+  } else {
+    localStorage.setItem("darkMode", "off");
+  }
+};
+
+window.onload = function () {
+  if (localStorage.getItem("darkMode") === "on") {
+    document.body.classList.add("dark-mode");
+  }
+};
